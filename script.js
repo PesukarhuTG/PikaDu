7b8206d562cc20f0568dda495df62db45cc9906d
Tatiana <ganzhinovat@yandex.ru>
2020-10-24 17:57:24 +0300

    Add feature for burger menu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const close = document.querySelector('.menu-toggle');
+const close = document.querySelector('.menu-active');
 const menu = document.querySelector('.sidebar');
 
 close.addEventListener('click', (e) => {
-    e.preventDefault();
     menu.classList.toggle('visible');
 });
