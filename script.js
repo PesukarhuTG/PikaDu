e8efb45a79ae3aeded7528e096377bda656d08ac
Tatiana <ganzhinovat@yandex.ru>
2020-11-12 14:22:21 +0300

    Add the oportunity to change the loginName and avatar

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ close.addEventListener('click', (e) => {
     menu.classList.toggle('visible');
 });
 
+
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
+
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
@@ -14,6 +17,14 @@ const loginSignup = document.querySelector('.login-signup');
 
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
+
+const exitElem = document.querySelector('.exit');
+const editContainer = document.querySelector('.edit-container');
+const editElem = document.querySelector('.edit');
+const editUsername = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+const userAvatarElem = document.querySelector('.user-avatar');
+
 
 const listUsers = [
     {
@@ -39,7 +50,14 @@ const setUsers = {
     user: null,
 
     logIn(email, password, handler) {
+
+        if(!regExpValidEmail.test(email)) {
+            alert('Не валидный email')
+            return;
+        }
+
         const user = this.getUser(email);
+
         if (user && user.password === password) {
             this.authorizedUser(user); //authorization
             handler(); //change blocks
@@ -48,11 +66,23 @@ const setUsers = {
         }
     },
 
-    logOut() {
-        console.log('выход');
+    logOut(handler) {
+        this.user = null;
+        handler(); //change blocks
     },
 
     signUp(email, password, handler) {
+
+        if(!regExpValidEmail.test(email)) {
+            alert('Не валидный email')
+            return;
+        }
+        
+        if (!email.trim() || !password.trim()) {
+            alert('Введите данные');
+            return;
+        }
+
         if (!this.getUser(email)) { 
             const user = {email, password, displayName: email.split('@')[0]}
             listUsers.push(user); //add new person
@@ -61,6 +91,18 @@ const setUsers = {
         } else {
             alert('Пользователь уже зарегистрирован');
         }
+    },
+
+    editUser(userName, userPhoto, handler) {
+        if (userName) {
+            this.user.displayName = userName;
+        }
+
+        if (userPhoto) {
+            this.user.photo = userPhoto;
+        }
+
+        handler();
     },
 
     getUser(email) {
@@ -72,16 +114,6 @@ const setUsers = {
     }
 }; 
 
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
-});
-
-loginSignup.addEventListener('click', (e) => {
-    e.preventDefault();
-    setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
-});
-
 //open login window after authorzation
 const toggleAuthDom = () => {
     const user = setUsers.user;
@@ -91,12 +123,47 @@ const toggleAuthDom = () => {
         loginElem.style.display = 'none';
         userElem.style.display = '';
         userNameElem.textContent = user.displayName;
+        userAvatarElem.src = user.photo || userAvatarElem.src;
     } else {
         loginElem.style.display = '';
         userElem.style.display = 'none';
     }
 
-}
+};
+
+
+
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
+    loginForm.reset();
+});
+
+loginSignup.addEventListener('click', (e) => {
+    e.preventDefault();
+    setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
+    loginForm.reset();
+});
+
+exitElem.addEventListener('click', (e) => {
+    e.preventDefault();
+    setUsers.logOut(toggleAuthDom);
+});
+
+editElem.addEventListener('click', (e) => {
+    e.preventDefault();
+    editContainer.classList.toggle('visible');
+    editUsername.value = setUsers.user.displayName;
+});
+
+editContainer.addEventListener('submit', (e) => {
+    e.preventDefault();
+    setUsers.editUser(editUsername.value, editPhotoURL.value, toggleAuthDom);
+    editContainer.classList.remove('visible')
+
+});
 
 
 toggleAuthDom();
